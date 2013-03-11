cf2b4ff8e36f4ced1a550291ded08194c4c80196
LArchaon <alex.buzdin@gmail.com>
2013-03-11 23:31:30 +0200

    Init commit

--- src/pl/byd/promand/Team3/MyActivity.java
+++ src/pl/byd/promand/Team3/MyActivity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MyActivity extends Activity {
+
+    /*
+     * Comment
+     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
