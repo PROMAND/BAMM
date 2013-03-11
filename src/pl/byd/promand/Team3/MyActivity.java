921e23a46c41d4fdb56414d067a941640dbe4957
LArchaon <alex.buzdin@gmail.com>
2013-03-11 23:29:17 +0200

    Init commit

--- src/pl/byd/promand/Team3/MyActivity.java
+++ src/pl/byd/promand/Team3/MyActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MyActivity extends Activity {
     /**
-     * Called when the activity is first created.
+     *
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
