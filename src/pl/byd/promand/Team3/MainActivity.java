83dafaeb71094e20ac2c8989eb034d63a25e8497
Tosha <zemskovsantons@gmail.com>
2013-03-14 14:29:32 +0200

    Started to make project model. Added new Activities and blank layouts.

--- src/pl/byd/promand/Team3/MainActivity.java
+++ src/pl/byd/promand/Team3/MainActivity.java
@@ -3,7 +3,7 @@ package pl.byd.promand.Team3;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity {
+public class MainActivity extends Activity {
 
     /*
      * Replacing comment
---
+++ src/pl/byd/promand/Team3/MenuActivity.java
@@ -0,0 +1,17 @@
+package pl.byd.promand.Team3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tosha
+ * Date: 14.03.13
+ * Time: 14:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class MenuActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
