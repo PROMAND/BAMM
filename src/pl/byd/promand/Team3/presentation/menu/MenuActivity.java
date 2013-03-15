e43eb84fe1b634a24980b83ce2bfce60ad525e02
Borys <borys.nexus@gmail.com>
2013-03-15 20:15:33 +0100

    connection between menu and order activity

--- src/pl/byd/promand/Team3/presentation/menu/MenuActivity.java
+++ src/pl/byd/promand/Team3/presentation/menu/MenuActivity.java
@@ -3,13 +3,17 @@ package pl.byd.promand.Team3.presentation.menu;
 
 import java.util.ArrayList;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ExpandableListView;
 import com.actionbarsherlock.app.SherlockActivity;
 import pl.byd.promand.Team3.R;
 import pl.byd.promand.Team3.infrastructure.menu.ExpandListAdapter;
 import pl.byd.promand.Team3.infrastructure.menu.ExpandListChild;
 import pl.byd.promand.Team3.infrastructure.menu.ExpandListGroup;
+import pl.byd.promand.Team3.presentation.order.OrderActivity;
 
 public class MenuActivity extends SherlockActivity {
     /** Called when the activity is first created. */
@@ -24,6 +28,7 @@ public class MenuActivity extends SherlockActivity {
         ExpListItems = SetStandardGroups();
         ExpAdapter = new ExpandListAdapter(MenuActivity.this, ExpListItems);
         ExpandList.setAdapter(ExpAdapter);
+
     }
 
     public ArrayList<ExpandListGroup> SetStandardGroups() {
@@ -48,6 +53,16 @@ public class MenuActivity extends SherlockActivity {
             group.setItems(temp_child_list);
             child_list.add(temp_child_list);
         }
+
+        Button but = (Button) findViewById(R.id.menuOrderButton);
+
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intend = new Intent(getApplicationContext(), OrderActivity.class);
+                startActivity(intend);
+            }
+        });
 
         return gru_list;
    }
