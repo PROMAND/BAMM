0413bfc6b148cea37185b004234113ad22e98b0a
Tosha <zemskovsantons@gmail.com>
2013-03-18 19:12:26 +0200

    some changes in Order view

--- src/pl/byd/promand/Team3/presentation/order/OrderActivity.java
+++ src/pl/byd/promand/Team3/presentation/order/OrderActivity.java
@@ -1,26 +1,37 @@
 package pl.byd.promand.Team3.presentation.order;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.*;
 import com.actionbarsherlock.app.SherlockActivity;
 import pl.byd.promand.Team3.R;
-import pl.byd.promand.Team3.infrastructure.order.OrderBean;
 
 public class OrderActivity extends SherlockActivity {
-
     private String name;
     private String phoneNumber;
-    private OrderBean order;
 
     private EditText nameET;
     private EditText phoneNumberET;
 
+    private TextView textViewTime;
+    private TextView textViewDate;
+
     private Button confirmButton;
     private Button datePickButton;
     private Button timePickButton;
+
+    private TimePickerDialog timePicker;
+    private DatePickerDialog datePickerDialog;
+
+
+    private Integer year;
+    private Integer month;
+    private Integer day;
+    private Integer hours;
+    private Integer minutes;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,26 +41,29 @@ public class OrderActivity extends SherlockActivity {
         datePickButton = (Button)findViewById(R.id.btnDatePicker);
         timePickButton = (Button)findViewById(R.id.btnTimePirkcer);
 
+        textViewDate = (TextView) findViewById(R.id.orderDate);
+        textViewTime = (TextView) findViewById(R.id.orderTime);
+
         nameET = (EditText)findViewById(R.id.orderEditTextName);
         phoneNumberET = (EditText)findViewById(R.id.orderEditTextPhoneNumber);
-
-        //Order = new OrderBean();
-
-        datePickButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
 
         timePickButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                timePicker = new TimePickerDialog(OrderActivity.this, onTimeSetListener,19,00,true);
+                timePicker.show();
 
             }
         });
 
+        datePickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePickerDialog = new DatePickerDialog(OrderActivity.this, onDateSetListener,2012,2,10);
+                datePickerDialog.show();
 
+            }
+        });
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -59,7 +73,7 @@ public class OrderActivity extends SherlockActivity {
                 String mailText = name + "" + phoneNumber;
 
                 Intent intent = new Intent(Intent.ACTION_SENDTO);
-                intent.setType("text/plain");
+                intent.setType("message/rfc822");
                 intent.putExtra(Intent.EXTRA_EMAIL, "tosha123@inbox.lv");
                 intent.putExtra(Intent.EXTRA_SUBJECT, "Table Reservation");
                 intent.putExtra(Intent.EXTRA_TEXT, mailText);
@@ -69,4 +83,68 @@ public class OrderActivity extends SherlockActivity {
         });
 
     }
+
+    private DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener(){
+
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+            setYear(year);
+            setMonth(month);
+            setDay(day);
+            textViewDate.setText(String.valueOf(year) + "/" + String.valueOf(month) + "/" + String.valueOf(day));
+
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener onTimeSetListener =
+            new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int hours, int minutes) {
+            setHours(hours);
+            setMinutes(minutes);
+            textViewTime.setText(String.valueOf(hours) + ":" + String.valueOf(minutes));
+        }
+    };
+
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public void setMonth(Integer month) {
+        this.month = month;
+    }
+
+    public Integer getDay() {
+        return day;
+    }
+
+    public void setDay(Integer day) {
+        this.day = day;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
+    public Integer getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(Integer minutes) {
+        this.minutes = minutes;
+    }
+
 }
