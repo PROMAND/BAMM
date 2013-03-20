73abd1bb42cf3796ba3ae969083076eeb5ae3fe0
Chipsas <mindaugasiv@gmail.com>
2013-03-20 14:26:29 +0100

    Some cosmetic updates

--- src/pl/byd/promand/Team3/presentation/menu/MenuActivity.java
+++ src/pl/byd/promand/Team3/presentation/menu/MenuActivity.java
@@ -1,5 +1,6 @@
 package pl.byd.promand.Team3.presentation.menu;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -120,4 +121,5 @@ public class MenuActivity extends SherlockActivity {
         getSupportMenuInflater().inflate(R.menu.menu_menu, menu);
         return true;
     }
+
 }
